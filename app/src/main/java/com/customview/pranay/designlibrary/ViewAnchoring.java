127d4672e391aeedc18f3b879609a4b3c5cc0182
pranay1494 <pranay1494@gmail.com>
2017-01-17 00:38:11 +0530

    + view anchoring added

---
+++ app/src/main/java/com/customview/pranay/designlibrary/ViewAnchoring.java
@@ -0,0 +1,16 @@
+package com.customview.pranay.designlibrary;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Pranay on 16-01-2017.
+ */
+public class ViewAnchoring  extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
